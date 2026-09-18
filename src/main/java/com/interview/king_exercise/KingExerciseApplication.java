bd12e452c79111602ca547c94d356b5160bd6ef5
Manuel García Cotes <71897980+Cotes08@users.noreply.github.com>
2026-09-18 17:36:32 +0200

    Initial commit

---
+++ src/main/java/com/interview/king_exercise/KingExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.interview.king_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/king_exercise/KingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.king_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
